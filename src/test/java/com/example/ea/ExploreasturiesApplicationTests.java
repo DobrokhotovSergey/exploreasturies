fca25373a8fd360a8af63d24b6b82b2f883c7593
Adrian Fernandez <a.fernandez@tesipro.com>
2017-02-13 17:49:18 +0100

    first shot

---
+++ src/test/java/com/example/ea/ExploreasturiesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.ea;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExploreasturiesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
